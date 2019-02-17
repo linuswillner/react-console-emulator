08a6006fa19a26dc0fb77db968bd31eca751f105
Linus Willner <lwtechgaming@gmail.com>
2019-02-18 00:01:11 +0200

    Update Jest config
    * Replace deprecated setupTestFrameworkScriptFile with setupFilesAfterEnv

--- jest.coverage.js
+++ jest.coverage.js
@@ -1,7 +1,7 @@
 // Jest config for additional coverage testing
 module.exports = {
   preset: 'jest-puppeteer',
-  setupTestFrameworkScriptFile: '<rootDir>/test/setupTests.js',
+  setupFilesAfterEnv: [ '<rootDir>/test/setupTests.js' ],
   collectCoverage: true,
   collectCoverageFrom: [ '<rootDir>/src/components/Terminal.jsx' ],
   coverageDirectory: './coverage/'

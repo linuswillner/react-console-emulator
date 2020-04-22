29e6a82dd92dbb5e42d034903099b0bda0310f96
Linus Willner <hello@linuswillner.me>
2020-04-22 20:44:21 +0300

    Fix extremely long-running history bug, refactor scrollHistory handler
    * Up until now, there had been an issue where when the history only had one unit in it, the terminal would not correctly reset the input value and would show the same value twice. This has now finally been fixed after having been in the library for over two years

--- src/handlers/scrollHistory.js
+++ src/handlers/scrollHistory.js
@@ -21,36 +21,73 @@ export default (direction, options) => {
 
   if (commandHistory.length > 0) { // Only run if history is non-empty and in use
     switch (direction) {
-      case 'up':
+      case 'up': {
+        // Declaring variables for these here to better clarify this block which can get pretty convoluted
+        const latest = commandHistory[0]
+        const first = commandHistory[commandHistory.length - 1]
+        const next = commandHistory[position + 1]
+
         if (position === null) {
-          // If at no position, get most recent entry
-          terminal.value = commandHistory[0]
-          return { historyPosition: 0, previousHistoryPosition: null }
+          // If at no yet defined position, get most recent entry
+          terminal.value = latest
+
+          return {
+            historyPosition: 0,
+            previousHistoryPosition: null
+          }
         } else if (position + 1 === commandHistory.length) {
           // If the first entry will be reached on this press, get it and decrement position by 1 to avoid confusing downscroll
-          terminal.value = commandHistory[commandHistory.length - 1]
-          return { historyPosition: commandHistory.length - 1, previousHistoryPosition: commandHistory.length - 2 }
+          // EXCEPT: If there is only 1 unit in the history, our previous position was actually null, not zero as defined above
+          // Hence why in one-unit histories the previous position has to be set to null, not 0
+          terminal.value = first
+
+          return {
+            historyPosition: commandHistory.length - 1,
+            previousHistoryPosition: commandHistory.length === 1 ? null : commandHistory.length - 2
+          }
         } else {
           // Normal increment by one
-          terminal.value = commandHistory[position + 1]
-          return { historyPosition: position + 1, previousHistoryPosition: position }
+          terminal.value = next
+
+          return {
+            historyPosition: position + 1,
+            previousHistoryPosition: position
+          }
         }
-      case 'down':
+      }
+      case 'down': {
+        // Declaring variables for these here to better clarify this block which can get pretty convoluted
+        const latest = commandHistory[0]
+        const empty = ''
+        const next = commandHistory[position - 1]
+
         if (position === null || !commandHistory[position]) {
           // If at initial or out of range, clear (Unix-like behaviour)
-          terminal.value = ''
-          return { historyPosition: null, previousHistoryPosition: null }
-        } else if (position - 1 === -1) {
-          // Clear because user pressed up once and is now pressing down again => clear or is reaching bottom
-          if (previousPosition === null || (position === 0 && previousPosition === 1)) terminal.value = ''
-          else terminal.value = commandHistory[0]
+          terminal.value = empty
 
-          return { historyPosition: null, previousHistoryPosition: null }
+          return {
+            historyPosition: null,
+            previousHistoryPosition: null
+          }
+        } else if (position - 1 === -1) {
+          // Clear because user is either pressing up once and is now pressing down again, or is reaching the latest entry
+          if (previousPosition === null || (position === 0 && previousPosition === 1)) terminal.value = empty
+          else terminal.value = latest
+
+          return {
+            historyPosition: null,
+            previousHistoryPosition: null
+          }
         } else {
           // Normal decrement by one
-          terminal.value = commandHistory[position - 1]
-          return { historyPosition: position - 1, previousHistoryPosition: position }
+          terminal.value = next
+
+          return {
+            historyPosition: position - 1,
+            previousHistoryPosition: position
+          }
         }
+      }
     }
   }
 }

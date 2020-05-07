d0957cd00bfeeff5b5714eaa24a33bb346df3bde
Linus Willner <hello@linuswillner.me>
2020-05-07 14:52:41 +0300

    Fix JSDOM

--- test/setupTests.js
+++ test/setupTests.js
@@ -2,7 +2,7 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { JSDOM } from 'jsdom'
 
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+const jsdom = new JSDOM('<!DOCTYPE html><html><body></body></html>', { url: 'http://localhost' })
 const { window } = jsdom
 
 global.window = window
